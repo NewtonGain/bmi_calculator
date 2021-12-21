bc57e1a012984f367fd04ce05fc0908fd2fa3d16
tanimSrabon13 <inamul.ke@gmail.com>
2021-12-21 15:46:26 +0600

    fdfsdff

--- lib/container.dart
+++ lib/container.dart
@@ -14,34 +14,67 @@ class _InputPageState extends State<InputPage> {
       centerTitle: true,
       ),
       body: Column(
-        children: [
-          Reuseable(color: Color(0xFF0F1337), iconContent: Icon(Icons.male), name: 'Femal', )
-        ],
-      )
+       children: [
+         Expanded(child: 
+         Row(
+           children: [
+             Expanded(child: Reuseable(color:Color(0xFF0F1337) ),),
+             Expanded(child: Reuseable(color:Color(0xFF0F1337) ),),
+             
+           ],
+           
+         ),
+         
+         ),
+         
+
+
+         
+          Expanded(child: 
+         Row(
+           children: [
+             Expanded(child: Reuseable(color:Color(0xFF0F1337) ),),
+            
+             
+           ],
+           
+         ),
+         
+         ),
+          Expanded(child: 
+         Row(
+           children: [
+             Expanded(child: Reuseable(color:Color(0xFF0F1337) ),),
+             Expanded(child: Reuseable(color:Color(0xFF0F1337) ),),
+             
+           ],
+           
+         ),
+         
+         ),
+        
+        
+       ],
+       
+      ),
     );
   }
 }
 
 class Reuseable extends StatelessWidget {
-  Reuseable({required this.color, required this.iconContent, required this.name});
+  Reuseable({required this.color, });
   final Color color;
-  final Widget iconContent;
-  final String name;
+  
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(15.0),
+        margin: EdgeInsets.all(2.0),
         decoration: BoxDecoration(
     color: Color(0xFF0F1337),
     borderRadius: BorderRadius.circular(13)
         ),
-        child: Column(
-          children: [
-            iconContent,
-            Text(name)
-          ],
-        )
+        
         
 
       );
